e6a66094ea4b6c9aa313a0c814dd5220210aaeb8
micah courey & marvin nikijuluw <student@epicodus.com>
2015-09-16 15:59:26 -0700

    create GameBoard method and pass tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,54 @@
-function Player(x, o) {
-  this.x = "X";
-  this.o = "O";
+function PlayerX(mark_x) {
+  this.mark_x = mark_x;
 };
 
-Player.prototype.mark = function() {
-  return this.x;
-  return this.o;
+PlayerX.prototype.mark = function() {
+  return this.mark_x;
 };
 
-function Space(x_coordinate, y_coordinate) {
-  this.x_coordinates = [1,2,3];
-  this.y_coordinates = [1,2,3];
+function PlayerO(mark_o) {
+  this.mark_o = mark_o;
 };
 
-Space.prototype.x_position = function() {
-  for(var i = 0; i < this.x_coordinates.length; i++) {
-    if(this.x_coordinates[i] == this.x_coordinate) {
-      return this.x_coordinate;
-    };
-  };
+PlayerO.prototype.mark = function() {
+  return this.mark_o;
 };
 
-Space.prototype.markby = function() {
-
-  return this.y_coordinate;
+function GameBoard() {
+  this.spaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 };
+
+GameBoard.prototype.mark = function(xo, position) {
+  this.spaces.splice(position, 1, xo);
+  return this.spaces;
+};
+
+
+GameBoard.prototype.win = function() {
+if (((this.spaces[0] == "X") && (this.spaces[1] == "X") && (this.spaces[2] == "X")) ||
+    ((this.spaces[3] == "X") && (this.spaces[4] == "X") && (this.spaces[5] == "X")) ||
+    ((this.spaces[6] == "X") && (this.spaces[7] == "X") && (this.spaces[8] == "X")) ||
+    ((this.spaces[0] == "X") && (this.spaces[3] == "X") && (this.spaces[6] == "X")) ||
+    ((this.spaces[1] == "X") && (this.spaces[4] == "X") && (this.spaces[7] == "X")) ||
+    ((this.spaces[2] == "X") && (this.spaces[5] == "X") && (this.spaces[8] == "X")) ||
+    ((this.spaces[0] == "X") && (this.spaces[4] == "X") && (this.spaces[8] == "X")) ||
+    ((this.spaces[2] == "X") && (this.spaces[4] == "X") && (this.spaces[6] == "X")));
+
+    return "Player X wins!"
+
+if (((this.spaces[0] == "O") && (this.spaces[1] == "O") && (this.spaces[2] == "O")) ||
+    ((this.spaces[3] == "O") && (this.spaces[4] == "O") && (this.spaces[5] == "O")) ||
+    ((this.spaces[6] == "O") && (this.spaces[7] == "O") && (this.spaces[8] == "O")) ||
+    ((this.spaces[0] == "O") && (this.spaces[3] == "O") && (this.spaces[6] == "O")) ||
+    ((this.spaces[1] == "O") && (this.spaces[4] == "O") && (this.spaces[7] == "O")) ||
+    ((this.spaces[2] == "O") && (this.spaces[5] == "O") && (this.spaces[8] == "O")) ||
+    ((this.spaces[0] == "O") && (this.spaces[4] == "O") && (this.spaces[8] == "O")) ||
+    ((this.spaces[2] == "O") && (this.spaces[4] == "O") && (this.spaces[6] == "O")));
+
+    return "Player O wins!"
+};
+
+// $(document).ready(function(){
+//   var gameBoard = new GameBoard();
+//   var currentthis.spaces = gameBoard.mark("X", 1);
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,23 @@
-describe('Player', function() {
+describe('PlayerX', function() {
   it("returns the player's mark", function() {
-    var testPlayer = new Player("X");
-    var testPlayer = new Player("O");
-    expect(testPlayer.mark()).to.equal("X", "O");
+    var testPlayerX = new PlayerX(3);
+    expect(testPlayerX.mark()).to.equal(3);
   });
 });
 
-describe("Space", function() {
-  it("returns the x_coordinate space", function() {
-    var testSpace = new Space(1,2);
-    expect(testSpace.x_position()).to.equal(1);
+describe("GameBoard", function() {
+  it("returns X for the 3rd position in the array", function() {
+    var testGameBoard = new GameBoard();
+    var currentSpaces = testGameBoard.mark("X", 1);
+    expect(currentSpaces).to.eql([0, "X", 2, 3, 4, 5, 6, 7, 8]);
+  });
+  it("returns a win if player picks 3 squares in a row", function() {
+    var testGameBoard = new GameBoard();
+    var currentSpace1 = testGameBoard.mark("X", 0);
+    var currentSpace2 = testGameBoard.mark("X", 4);
+    var currentSpace3 = testGameBoard.mark("X", 8);
+    var winScenario = testGameBoard.win(currentSpace3);
+    expect(winScenario).to.equal("Player X wins!");
   });
 
-  // it("returns the y_coordinate space", function() {
-  //   var testSpace = new Space(1,3);
-  //   expect(testSpace.markby()).to.equal(3);
-  // });
 });
