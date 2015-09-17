87da3d7c6f8563a42ecdd7b145bcc68782ac5aa2
marvin nikijuluw & micah courey <student@epicodus.com>
2015-09-17 10:32:06 -0700

    check wins jQuery works

--- js/scripts.js
+++ js/scripts.js
@@ -16,34 +16,51 @@ GameBoard.prototype.mark = function(xo, position) {
 
 //winning scenario
 GameBoard.prototype.win = function() {
-if (((this.spaces[0] == "X") && (this.spaces[1] == "X") && (this.spaces[2] == "X")) ||
+if (
+    ((this.spaces[0] == "X") && (this.spaces[1] == "X") && (this.spaces[2] == "X")) ||
     ((this.spaces[3] == "X") && (this.spaces[4] == "X") && (this.spaces[5] == "X")) ||
     ((this.spaces[6] == "X") && (this.spaces[7] == "X") && (this.spaces[8] == "X")) ||
     ((this.spaces[0] == "X") && (this.spaces[3] == "X") && (this.spaces[6] == "X")) ||
     ((this.spaces[1] == "X") && (this.spaces[4] == "X") && (this.spaces[7] == "X")) ||
     ((this.spaces[2] == "X") && (this.spaces[5] == "X") && (this.spaces[8] == "X")) ||
     ((this.spaces[0] == "X") && (this.spaces[4] == "X") && (this.spaces[8] == "X")) ||
-    ((this.spaces[2] == "X") && (this.spaces[4] == "X") && (this.spaces[6] == "X")));
+    ((this.spaces[2] == "X") && (this.spaces[4] == "X") && (this.spaces[6] == "X")))
+    {
+       return "Player X wins!"
+    }
 
-    return "Player X wins!"
-
-if (((this.spaces[0] == "O") && (this.spaces[1] == "O") && (this.spaces[2] == "O")) ||
+if (
+    ((this.spaces[0] == "O") && (this.spaces[1] == "O") && (this.spaces[2] == "O")) ||
     ((this.spaces[3] == "O") && (this.spaces[4] == "O") && (this.spaces[5] == "O")) ||
     ((this.spaces[6] == "O") && (this.spaces[7] == "O") && (this.spaces[8] == "O")) ||
     ((this.spaces[0] == "O") && (this.spaces[3] == "O") && (this.spaces[6] == "O")) ||
     ((this.spaces[1] == "O") && (this.spaces[4] == "O") && (this.spaces[7] == "O")) ||
     ((this.spaces[2] == "O") && (this.spaces[5] == "O") && (this.spaces[8] == "O")) ||
     ((this.spaces[0] == "O") && (this.spaces[4] == "O") && (this.spaces[8] == "O")) ||
-    ((this.spaces[2] == "O") && (this.spaces[4] == "O") && (this.spaces[6] == "O")));
-
+    ((this.spaces[2] == "O") && (this.spaces[4] == "O") && (this.spaces[6] == "O")))
+    {
     return "Player O wins!"
+  }
 };
 
-// $(document).ready(function(){
-//   var gameBoard = new GameBoard();
-//
-//   var playerX = new Player("X");
-//   var playerO = new Player("O");
-//
-//   gameBoard.mark("X", 1);
-// });
+$(document).ready(function(){
+  var gameBoard = new GameBoard();
+
+  $("#form_X").submit(function(event) {
+    event.preventDefault();
+    var markX = $("input#X").val();
+    gameBoard.mark("X", markX);
+    var checkWin = gameBoard.win();
+    console.log(checkWin);
+    console.log(gameBoard.spaces);
+  });
+
+  $("#form_O").submit(function(event) {
+    event.preventDefault();
+    var markO = $("input#O").val();
+    gameBoard.mark("O", markO);
+    var checkWin = gameBoard.win();
+    console.log(checkWin);
+    console.log(gameBoard.spaces);
+   });
+});
